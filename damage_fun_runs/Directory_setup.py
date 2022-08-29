41910b7dadbf51f2d22368c10c308a2002d23213
Jonah Gilbert <jonahmgilbert1@gmail.com>
2022-08-29 14:47:17 -0500

    Update Directory_setup.py

--- damage_fun_runs/Directory_setup.py
+++ damage_fun_runs/Directory_setup.py
@@ -39,7 +39,8 @@ conf_base = {'mortality_version': 1,
   'energy': {'formula': 'damages ~ -1 + anomaly + np.power(anomaly, 2)'},
   'labor': {'formula': 'damages ~ -1 + anomaly + np.power(anomaly, 2)'},
   'AMEL_m1': {'formula': 'damages ~ -1 + anomaly + np.power(anomaly, 2)'},
-  'CAMEL_m1_c0.20': {'formula': 'damages ~ -1 + anomaly + np.power(anomaly, 2) + gmsl + np.power(gmsl, 2)'}}}
+  'CAMEL_m1_c0.20': {'formula': 'damages ~ -1 + anomaly + np.power(anomaly, 2) + gmsl + np.power(gmsl, 2)'}},
+  'save_path': str(outputs)}
 
 sectors = list(conf_base['sectors'].keys())
 for i in sectors:
